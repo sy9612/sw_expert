fb134812e0f5256dd267dc7656d46d86bdce0f87
sy9612 <sy9612@naver.com>
2020-09-14 16:54:23 +0900

    0914

--- BOJ_11048.java
+++ BOJ_11048.java
@@ -18,7 +18,7 @@ public class BOJ_11048 {
 
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= M; j++) {
+			for (int j = 1; j <= M; j++) { 
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
