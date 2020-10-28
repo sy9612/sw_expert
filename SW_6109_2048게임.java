28f2297965efb5ffa611117d5fbfc25f16b134e6
sy9612 <sy9612@naver.com>
2020-10-29 00:40:47 +0900

    1028

--- SW_6109_2048게임.java
+++ SW_6109_2048게임.java
@@ -38,51 +38,134 @@ public class SW_6109_2048게임 {
 	}
 
 	public static void solve(String S) {
+		boolean flag = true;
 		if (S.equals("right")) {
-			ArrayList<Integer> arr = new ArrayList();
-			for(int i=0; i<N; i++) {
-				for(int j=N-1; j >= 0; j--) {
-					if(arr.size() > 0 && arr.get(arr.size() - 1) == map[i][j]) {
+			for (int i = 0; i < N; i++) {
+				ArrayList<Integer> arr = new ArrayList();
+
+				for (int j = N - 1; j >= 0; j--) {
+					if (map[i][j] == 0)
+						continue;
+					if (flag && arr.size() > 0 && arr.get(arr.size() - 1) == map[i][j]) {
 						arr.remove(arr.size() - 1);
-						arr.add(map[i][j] * 2) ;
+						arr.add(map[i][j] * 2);
+						flag = false;
+					} else {
+						arr.add(map[i][j]);
+						flag = true;
 					}
-					else
-						arr.add(map[i][j]) ;
 				}
-				
+
 				int idx = 0;
-				for(int j=N-1; j >= 0; j--) {
-					while(arr.isEmpty()) {
-					map[i][j] = arr.get(idx++);
-					}
+				for (int j = N - 1; j >= 0; j--) {
+					if (idx < arr.size()) {
+						map[i][j] = arr.get(idx++);
+					} else
+						map[i][j] = 0;
 				}
 			}
-		}//right
+		} // right
 
 		if (S.equals("left")) {
+			for (int i = 0; i < N; i++) {
+				ArrayList<Integer> arr = new ArrayList();
 
-		}//left
+				for (int j = 0; j < N; j++) {
+					if (map[i][j] == 0)
+						continue;
+					if (flag && arr.size() > 0 && arr.get(arr.size() - 1) == map[i][j]) {
+						arr.remove(arr.size() - 1);
+						arr.add(map[i][j] * 2);
+						flag = false;
+					} else {
+						arr.add(map[i][j]);
+						flag = true;
+					}
+				}
+
+				int idx = 0;
+				for (int j = 0; j < N; j++) {
+					if (idx < arr.size()) {
+						map[i][j] = arr.get(idx++);
+					} else
+						map[i][j] = 0;
+				}
+			}
+		} // left
 
 		if (S.equals("up")) {
+			for (int i = 0; i < N; i++) {
+				ArrayList<Integer> arr = new ArrayList();
 
-		}
+				for (int j = 0; j < N; j++) {
+					if (map[j][i] == 0)
+						continue;
+					if (flag && arr.size() > 0 && arr.get(arr.size() - 1) == map[j][i]) {
+						arr.remove(arr.size() - 1);
+						arr.add(map[j][i] * 2);
+						flag = false;
+					} else {
+						arr.add(map[j][i]);
+						flag = true;
+					}
+				}
+
+				int idx = 0;
+				for (int j = 0; j < N; j++) {
+					if (idx < arr.size()) {
+						map[j][i] = arr.get(idx++);
+					} else
+						map[j][i] = 0;
+				}
+			}
+		} // up
 
 		if (S.equals("down")) {
+			for (int i = 0; i < N; i++) {
+				ArrayList<Integer> arr = new ArrayList();
 
-		}
+				for (int j = N - 1; j >= 0; j--) {
+					if (map[j][i] == 0)
+						continue;
+					if (flag && arr.size() > 0 && arr.get(arr.size() - 1) == map[j][i]) {
+						arr.remove(arr.size() - 1);
+						arr.add(map[j][i] * 2);
+						flag = false;
+					} else {
+						arr.add(map[j][i]);
+						flag = true;
+					}
+				}
 
-	}//solve
+				int idx = 0;
+				for (int j = N - 1; j >= 0; j--) {
+					if (idx < arr.size()) {
+						map[j][i] = arr.get(idx++);
+					} else
+						map[j][i] = 0;
+				}
+			}
+		} // down
+
+	}// solve
 }
 
-
 /*
-1
-4 right
-2 0 2 4
-2 0 4 2
-2 2 4 8
+ * 1 4 right 2 0 2 4 2 0 4 2 2 2 4 8 2 2 4 4
+ * 
+ * 1 4 left 2 0 2 4 2 0 4 2 2 2 4 8 2 2 4 4
+ * 
+1 
+4 up 
+2 0 2 4 
+2 0 4 2 
+2 2 4 8 
 2 2 4 4
-
- */
-
-
+1 
+4 down 
+2 0 2 4 
+2 0 4 2 
+2 2 4 8 
+2 2 4 4
+*/
+ 
