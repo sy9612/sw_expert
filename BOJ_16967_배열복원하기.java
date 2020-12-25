a0f9ce79836debe4b4ef1d0e47f287621bca16ed
lsy <lsy@218.38.137.27>
2020-12-25 16:06:38 +0900

    1225

---
+++ BOJ_16967_배열복원하기.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class BOJ_16967_배열복원하기 {
+	static int H, W, X, Y;
+	static int A[][], B[][];
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		H = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+		Y = Integer.parseInt(st.nextToken());
+
+		A = new int[H][W];
+		B = new int[H + X][W + Y];
+
+		for (int i = 0; i < H + X; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < W + Y; j++) {
+				B[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		int i = 0, j = 0, k = 0, l = 0;
+		for (i = 0; i < H; i++) {
+			for (j = 0; j < W; j++) {
+				if(i<X && j < W)
+					A[i][j] = B[i][j];
+				else if(i < H && j < Y)
+					A[i][j] = B[i][j];
+				else if(i >= X && j >= Y)
+					A[i][j] = B[i][j] - A[i-X][j-Y];
+			}
+		}
+
+		for (i = 0; i < H; i++) {
+			for (j = 0; j < W; j++) {
+				System.out.print(A[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+/*
+ 2 4 1 1
+1 2 3 4 0
+5 7 9 11 4
+0 5 6 7 8
+
+2 2 1 1
+1 2 0
+3 5 2
+0 3 4
+*/
