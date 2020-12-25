d471c439f37fccb4dd6fb9239541f492eb77194c
sy9612 <sy9612@218.38.137.27>
2020-12-25 16:15:57 +0900

    1225

--- BOJ_16967_배열복원하기.java
+++ BOJ_16967_배열복원하기.java
@@ -26,12 +26,12 @@ public class BOJ_16967_배열복원하기 {
 		int i = 0, j = 0, k = 0, l = 0;
 		for (i = 0; i < H; i++) {
 			for (j = 0; j < W; j++) {
-				if(i<X && j < W)
+				if (i < X && j < W)
 					A[i][j] = B[i][j];
-				else if(i < H && j < Y)
+				else if (i < H && j < Y)
 					A[i][j] = B[i][j];
-				else if(i >= X && j >= Y)
-					A[i][j] = B[i][j] - A[i-X][j-Y];
+				else if (i >= X && j >= Y)
+					A[i][j] = B[i][j] - A[i - X][j - Y];
 			}
 		}
 
