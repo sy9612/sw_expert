cfcbf7a4a9c033645835045b2830b1898ffc0160
sy9612 <sy9612@naver.com>
2021-12-01 23:03:47 +0900

    Create PG_순위검색.java

---
+++ PROGRAMMERS/PG_순위검색.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public static int[] solution(String[] info, String[] query) {
+        int infoLen = info.length;
+        int queryLen = query.length;
+        int[] answer = new int[queryLen];
+        
+        String language[] = {"cpp","java","python"};
+        String position[] = {"backend","frontend"};
+        String year[] = {"junior","senior"};
+        String food[] = {"chicken","pizza"};
+        
+        
+        return answer;
+    }
+
+}
